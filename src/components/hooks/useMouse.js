bcdccbddbec492c772b1243411c62253599465f0
Vladimir <freshitpte@gmail.com>
2020-11-01 14:25:16 +0200

    create useMouse hook

---
+++ src/components/hooks/useMouse.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function useHookMouse() {
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
+  const handleMove = ({ clientX: x, clientY: y }) => { setMouse({ x, y }) };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMove);
+    }
+  }, [])
+
+  return { x: mouse.x, y: mouse.y }
+}
+
+export default useHookMouse;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 import Mouse from '../../components/Mouse';
 import Counter from '../../components/FuncCounter';
 import FuncMouse from '../../components/FuncMouse';
+import useHookMouse from '../../components/hooks/useMouse';
 
 function Home() {
   const [isShown, setIsShown] = useState(true);
+  const { x, y } = useHookMouse();
+
   return (
     <div
       style={{ minWidth: '10px', minHeight: '10px', backgroundColor: 'red' }}
       // onClick={() => setIsShown(!isShown)}
-      >
-      {isShown && <FuncMouse />}
-      <Counter/>
+    >
+      {isShown && (
+        <h1>
+          X: {x}, Y: {y}
+        </h1>
+      )}
+      <Counter />
     </div>
   );
 }
